93e8f94a278828b49a951502828acdf913b94d1d
Adam Back <adb33@email.arizona.edu>
2014-10-13 15:03:24 -0700

    Started adding comment logic. Refactored how page handles submit. Refactoring some of the basic functionality.

--- app.js
+++ app.js
@@ -2,25 +2,51 @@ $(document).ready(function() {
   //on page load, store a whole bunch of comments from a popular video on YouTube
   // page for commented videos http://vidstatsx.com/most-commented-videos-all-time
   var comments;
+  // English, limit swearing
+  var vettedComments;
+
   $.ajax({
     type: 'GET',
     url: "https://gdata.youtube.com/feeds/api/videos/_OBlgSz8sSM/comments?orderby=published",
     success: function(data) {
-      comments = data;
-      console.log(comments);
+      comments = $(data).find('content');
     },
     error: function(error) {
       console.error(error);
     }
   });
 
-  $('input').click('#submit', function(event) {
+  $('#submit').click(function(event) {
     event.preventDefault();
+    console.log('message');
     var sentence = $('#sentence').val()
     var addSentence = "<span class='userSentence'> " + sentence + "</span>"
+    //append it to the DOM 
     $('div.story').append(addSentence);
     $('#sentence').val('');
-    //get sentence to add using API
-    //append it to the DOM 
   });
+
+  var addSentence = function(text) {
+    // look for ending punctuation
+    switch(text.charAt(text.length - 1)) {
+      case '.':
+        break;
+      case '!':
+        break;
+      case '?':
+        break;
+      default: 
+        // if no ending punctuation, add some
+        text = text + ".";
+    };
+
+    var sentence = "<span class='userSentence'> " + text + "</span>"
+    $('div.story').append(sentence);
+  };
+
+  var writeMagically = function() {
+    var comment = comments[0].innerHTML;
+    addSentence(comment);
+  };
+
 });
