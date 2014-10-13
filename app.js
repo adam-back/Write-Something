80e7449b0f2cc7ab7475b24733e8353c65579fda
Adam Back <adb33@email.arizona.edu>
2014-10-13 14:38:53 -0700

    Successfully make GET request to youtube for comment stream.

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
+  //on page load, store a whole bunch of comments from a popular video on YouTube
+  // page for commented videos http://vidstatsx.com/most-commented-videos-all-time
+  var comments;
+  $.ajax({
+    type: 'GET',
+    url: "https://gdata.youtube.com/feeds/api/videos/_OBlgSz8sSM/comments?orderby=published",
+    success: function(data) {
+      comments = data;
+      console.log(comments);
+    },
+    error: function(error) {
+      console.error(error);
+    }
+  });
+
   $('input').click('#submit', function(event) {
     event.preventDefault();
     var sentence = $('#sentence').val()
     var addSentence = "<span class='userSentence'> " + sentence + "</span>"
     $('div.story').append(addSentence);
-    $('#sentence').val(''); 
+    $('#sentence').val('');
+    //get sentence to add using API
+    //append it to the DOM 
   });
 });
