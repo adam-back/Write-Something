f5adbe39fd30d36b7bd65827f87bea5dff5547b2
Adam Back <adb33@email.arizona.edu>
2014-10-13 19:59:30 -0700

    Basic server.

--- server-config.js
+++ server-config.js
@@ -1,1 +1,11 @@
-server-config.js
+var express = require('express');
+var partials = require('express-partials');
+
+var app = express();
+
+// app.use(partials());
+// app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World');
+});
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var app = require('./server-config.js');
+var express = require('express')
+var app = express()
 
 var port = process.env.PORT || 4568;
 
-app.listen(port);
 
+app.listen(port);
 console.log('Server now listening on port ' + port);
+
+app.use(express.static(__dirname + '/public'));
