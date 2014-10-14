ce725a2fc8d22069baa67177ee7159a13643cb3d
Adam Back <adb33@email.arizona.edu>
2014-10-14 15:41:42 -0700

    Splits sentences from story by period.

--- stories.js
+++ stories.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
 
   //loads Siddhartha to begin with as default.
   $.get('./Text/Siddhartha.txt', function(data) {
-    console.log('data', data);
+    var sentences = data.split('.');
+    sentences.forEach(function(value, index, array) {
+      ghostSentences.push(value + ".");
+    });
   });
 
   $("input[type='radio']").change(function() {
