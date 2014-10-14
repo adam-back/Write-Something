012203a14fd4bbd8a4bda880d20e5238708724a7
Adam Back <adb33@email.arizona.edu>
2014-10-14 15:33:30 -0700

    Ajax request reads from Siddhartha file immediately upon page load.

--- stories.js
+++ stories.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-  
+  var ghostSentences = [];
+
+  //loads Siddhartha to begin with as default.
+  $.get('./Text/Siddhartha.txt', function(data) {
+    console.log('data', data);
+  });
+
   $("input[type='radio']").change(function() {
     console.log('changed radio button');
   });
